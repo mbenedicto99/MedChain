29c69a64fed84e3f9e240e4099e001554a28315b
Marcos de Benedicto <mbenedicto@gmail.com>
2017-05-27 08:49:55 -0300

    MedChain_put.py
    Insert frame 1x300 data in mosaic ID 20hgfdsk3kbnhg453bkg45

--- MedChain_put.py
+++ MedChain_put.py
@@ -3,15 +3,16 @@ import requests
 import json
 
 #specify url
-url = 'my URL'
+url = 'www.eboxx.com.br/pub/MedChain/ledger'
 
-token = "my token"
+token = "742974fnsjh380vn38fnl4238vhn488v0s0nnrdhf83nl8yc38cn3"
 
 data = {
-        "agentName": "myAgentName",
-        "agentId": "20",
-        "description": "Changed Description",
-        "platform": "Windows"
+        "agentName": "MedChain",
+        "mosaicId": "20hgfdsk3kbnhg453bkg45",
+        "description": "DataIN",
+        "msg": "onoononoonononooonononono"
+        "frameid": "1x300"
         }
 
 headers = {'Authorization': 'Bearer ' + token, "Content-Type": "application/json", data:data}
