051de5a82b5ff3c216a323c0ec2dcfd9bad7be19
Marcos de Benedicto <mbenedicto@gmail.com>
2017-05-27 08:54:41 -0300

    Update MedChain_put.py

--- MedChain_put.py
+++ MedChain_put.py
@@ -1,8 +1,6 @@
-#import requests library for making REST calls
 import requests
 import json
 
-#specify url
 url = 'www.eboxx.com.br/pub/MedChain/ledger'
 
 token = "742974fnsjh380vn38fnl4238vhn488v0s0nnrdhf83nl8yc38cn3"
@@ -15,7 +13,7 @@ data = {
         "frameid": "1x300"
         }
 
-headers = {'Authorization': 'Bearer ' + token, "Content-Type": "application/json", data:data}
+headers = {'Authorization': 'Bearer ' + token, "Content-Type": "application/json", 'data':data}
 
 #Call REST API
 response = requests.put(url, data=data, headers=headers)
